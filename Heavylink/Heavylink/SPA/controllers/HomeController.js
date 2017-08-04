ab7f79b3069cb3aa91706ada303a3ffd2d065e59
Ognjen Gatalo <ognjeng95@gmail.com>
2017-08-05 01:51:55 +0200

    Uradio dodavanje, i brisanje iz arraya, takodje se dodaje na ctrl+v automatski u array

--- Heavylink/Heavylink/SPA/controllers/HomeController.js
+++ Heavylink/Heavylink/SPA/controllers/HomeController.js
@@ -1,10 +1,48 @@
 ﻿app.controller('HomeController', function ($scope, $window, $rootScope, HomeFactory) {
 
     function init() {
-        $scope.hello = "Hello from HomeController!";
+        $scope.links = [];
+        $scope.linkId = 0;
+        $scope.firstHotkey = 0;
     }
 
     init();
 
+    $scope.CallAddLinkOnEnter = function ($event) {
+        var key = $event.keyCode;
+        if (key == 13) {
+            $scope.addLink($scope.link);
+        }
+        else if(key == 17) {
+            $scope.firstHotkey = key;
+        }
+        else if(key == 86) {
+            if($scope.firstHotkey == 17) {
+                $scope.addLink($scope.link);
+                $scope.firstHotkey = 0;
+            }
+        }
+    }
+
+    $scope.addLink = function(link) {
+        if(link.pastedUrl == null || link.pastedUrl == '') {
+            alert('Please enter a valid link');
+            return;
+        }
+        if($scope.links.length == 10) {
+            alert('No more than 10 links per group!');
+            return;
+        }
+        link.id = $scope.linkId++;
+        $scope.links.push(link);
+        $scope.link = {};
+    }
+
+    $scope.removeLink = function(id,$event) {
+        $event.preventDefault();
+        $scope.links = $scope.links.filter(function( obj ) {
+            return obj.id !== id;
+        });
+    }
     
 })
