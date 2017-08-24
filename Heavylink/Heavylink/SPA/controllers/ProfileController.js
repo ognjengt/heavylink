384cdb0d1ab4ed4f8461de342bd7bf5b36199072
Ognjen Gatalo <ognjeng95@gmail.com>
2017-08-24 17:22:42 +0200

    Uradio tooltip, promeniti samo na $timeout, i ogranicenje za naslov

--- Heavylink/Heavylink/SPA/controllers/ProfileController.js
+++ Heavylink/Heavylink/SPA/controllers/ProfileController.js
@@ -4,12 +4,14 @@ app.controller('ProfileController', function ($scope, $window, $rootScope, Profi
     if(!$rootScope.loggedIn) {
       $location.path('/login');
     }
-    $('[data-toggle="tooltip"]').tooltip();
     var tokenPayload = jwtHelper.decodeToken(localStorage.getItem('token'));
     $scope.username = tokenPayload.unique_name;
 
     ProfileFactory.GetUserLinks($scope.username).then((response) => {
       $scope.userLinks = response.data;
+      setTimeout(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+      },1000)
     })
   }
 
